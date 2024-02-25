cd48be32801b3d0a8539869d07dab00015d539a6
sicktooth <sosaronungwe@gmail.com>
2024-02-25 20:51:16 +0100

    add js file, updated index.html

---
+++ index.js
@@ -0,0 +1,11 @@
+var submit = document.querySelector(".submit");
+var day = document.getElementById('day');
+var month = document.getElementById('month');
+var year = document.getElementById('year');
+
+submit.addEventListener("click", results);
+function results() {
+    // for (var i = 0; i < 3; i++) {
+    //   document.querySelectorAll("span")[i].textContent = "Fuck!";
+    // }
+}
