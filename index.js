73fca04ede9e37bca313f43c8d57a16a4096e6d9
sicktooth <sosaronungwe@gmail.com>
2024-02-26 04:49:14 +0100

    update index.html & .js

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ function results() {
     // for (var i = 0; i < 3; i++) {
     //   document.querySelectorAll("span")[i].textContent = "Fuck!";
     // }
+    var today = new Date();
+    var thisYear = today.getFullYear();
+    var thisMonth = today.getMonth;
+    var thisDay = today.getDate;
+    // document.querySelectorAll("span")[0].textContent = thisYear;
+    if (day == thisDay) {
+        console.log("Day checked out");
+    } else {
+        console.log("Day not checked out");
+    }
+    if (month == thisMonth) {
+        console.log("Month checked out");
+    } else {
+        console.log("Month not checked out");
+    }
+    if (year == thisYear) {
+        console.log("Year checked out");
+    } else {
+        console.log("Year not checked out");
+    }
+
 }
