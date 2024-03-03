6505776a005c0f08fdf0ebb545ed59746d934a3e
sicktooth <sosaronungwe@gmail.com>
2024-03-03 10:48:43 +0100

    update index.js

--- index.js
+++ index.js
@@ -25,10 +25,22 @@ function results(e) {
 
     if (thisYear == year) {
         // let resultYear = 0;
-        if (month == thisMonth) {
+        if (month > totalMonths || month <= 0) {
+            if (day > totalDays) {
+                alert('Please select a day in the month'); // returns error message
+            }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
+            }
+            alert('Please select a valid month'); // returns error message
+        }
+        else if (month == thisMonth) {
             document.querySelector(".monthResult").textContent = "0";
             if (day > totalDays) {
                 alert('Please select a day in the month'); // returns error message
+            }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
             }
             else if (day > thisDay) {
                 document.querySelector(".daysResult").textContent = 0; // Replace with error message
@@ -44,6 +56,9 @@ function results(e) {
         else if (month < thisMonth) {
             if (day > totalDays) {
                 alert('Please select a day in the month'); // returns error message and resets previous values
+            }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
             }
            else if (day > thisDay) {
                 document.querySelector(".daysResult").textContent = beforeRDay0;
@@ -71,6 +86,9 @@ function results(e) {
             if (day > totalDays) {
                 alert("Please select a valid day"); // returns error message
             }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
+            }
             alert("Please select a month in the past"); // returns error message
         }
 
@@ -78,10 +96,22 @@ function results(e) {
     
     else if (year < thisYear) {
         // if month > totalMonths returns error message
-        if (month == thisMonth) {
+        if (month > totalMonths || month <= 0) {
+            if (day > totalDays) {
+                alert('Please select a day in the month'); // returns error message
+            }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
+            }
+            alert('Please select a valid month'); // returns error message
+        }
+        else if (month == thisMonth) {
             // monthText = "0";
             if (day > totalDays) {
                 alert('Please select a day in the month'); // returns error message
+            }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
             }
             else if (day > thisDay) {
                 document.querySelector(".yearResult").textContent = (resultYear) - 1;
@@ -102,6 +132,9 @@ function results(e) {
             if (day > totalDays) {
                 alert('Please select a day in the month'); // returns error message
             }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
+            }
             else if (day > thisDay) {
                 document.querySelector(".yearResult").textContent = resultYear;
                 if (beforeRDay0 >= 30) {
@@ -121,9 +154,13 @@ function results(e) {
                 document.querySelector(".monthResult").textContent = resultMonth;
                 document.querySelector(".daysResult").textContent = "0";
             }
-        } else {
+        } 
+        else {
             if (day > totalDays) {
                 alert('Please select a day in the month'); // returns error message
+            }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
             }
             else if (day > thisDay) {
                 document.querySelector(".yearResult").textContent = (resultYear) - 1;
@@ -140,7 +177,18 @@ function results(e) {
                 document.querySelector(".daysResult").textContent = "0";
             }
         }
-    } else {
+    } 
+    
+    else {
+        if (month > totalMonths || month <= 0) {
+            if (day > totalDays) {
+                alert('Please select a day in the month'); // returns error message
+            }
+            else if (day <= 0) {
+                alert('Please select a valid day in the month'); // returns error message
+            }
+            alert('Please select a valid month'); // returns error message
+        }
         alert ("Input a year in the past!!!!!");
     }
 
